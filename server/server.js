daaa9308e2c0bec6473591eb5a9d10cb99fec41f
code-islah <codeislah@gmail.com>
2026-06-26 11:53:14 +0600

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import {Server} from 'socket.io';
+import express from "express";
+import http from "http";
+
+
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {origin: "*"}
+});
+
+io.on('connection', (socket) => {
+  console.log('User Connected: ', socket.id);
+  
+  // User থেকে "send_message" শুনো
+  socket.on('send_message', (data) => {
+    console.log('Message:', data.text);
+    
+    // io.emit = Room এর সবাইকে পাঠাও। তুমি সহ!
+    io.emit('notification', { message: `User said: ${data.text}` });
+  });
+});
+
+server.listen(3000, ()=>{
+    console.log("Pulse server live on Port: 3000");
+});
